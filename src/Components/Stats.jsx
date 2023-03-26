985ca26eea11192491cbe43ccdeb9574190b397d
Akingboye Marto <martinsakingboye8@gmail.com>
2023-03-26 17:06:09 +0100

    first commit

---
+++ src/Components/Stats.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { stats } from "../constants/index"
+import styles from '../style'
+
+const Stats = () => {
+  // console.log(stats)
+  return (
+    <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
+      {stats.map((item) => {
+        return <div
+          key={item.id}
+          className={`flex-1 flex justify-start items-center flex-row m-3`}
+        >
+          <h4 className=' font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[40px] text-white'>
+            {item.value}
+          </h4>
+          <p className='font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[26px] leading-[21px] text-gradient uppercase ml-3'>
+            {item.title}
+          </p>
+        </div>
+      }
+      )}
+    </section>
+  )
+}
+
+export default Stats
